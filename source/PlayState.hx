047d29720326ade082951a559b5be1fa3a1a332b
Dohxi <dohxis@gmail.com>
2014-12-06 09:46:23 +0200

    Mindstorming ended

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -6,9 +6,22 @@ import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.util.FlxMath;
+import flixel.util.FlxColor;
+import flixel.tile.FlxTilemap;
+import flixel.group.FlxGroup;
 
 class PlayState extends FlxState {
+
+	private static var isDead:Bool = false;
+	private var level:FlxTilemap;
+	private var player:FlxSprite;
+	private var score:FlxText;
+	private var objects:FlxGroup;
+	private var text:FlxText;
+
 	override public function create():Void {
+		FlxG.cameras.bgColor = FlxColor.WHITE;
+		FlxG.mouse.visible = false;
 		super.create();
 	}
 	
